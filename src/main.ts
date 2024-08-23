dff017e518fef85aa8739a05c6d5ff8c982d75a8
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-23 00:27:05 -0400

    Refactor main.ts and export functions
    Improve code style, use consistent quotes, and export parseString
    and parse functions for better module usage. Remove unnecessary
    export statement.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import Parser from './parser';
-import Lexer from './lexer';
-import * as fs from 'fs';
+import Parser from "./parser";
+import Lexer from "./lexer";
+import * as fs from "fs";
 
 /**
  * Returns the abstract syntax tree for a given string of QASM code.
  * @param qasm - The code string.
  * @return The corresponding AST.
  */
- function parseString(qasm:string) {
-    const lexer = new Lexer(qasm, 0);
-    const tokens = lexer.lex();
-    const parser = new Parser(tokens);
-    const ast = parser.parse();
-    return ast; 
+export function parseString(qasm: string) {
+  const lexer = new Lexer(qasm, 0);
+  const tokens = lexer.lex();
+  const parser = new Parser(tokens);
+  const ast = parser.parse();
+  return ast;
 }
 
 /**
@@ -20,8 +20,6 @@ import * as fs from 'fs';
  * @param file - The file location.
  * @return The corresponding AST.
  */
-exports.parse = function(file:string) {
-    return parseString(fs.readFileSync(file, 'utf8'));
+export function parse(file: string) {
+  return parseString(fs.readFileSync(file, "utf8"));
 }
-
-exports.parseString = parseString;
