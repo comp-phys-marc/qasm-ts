27e9be2206e4dd75c33abea568137c319fb3eac5
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-11-03 10:36:08 -0500

    Fix syntax error on inverseLookup call

--- src/qasm2/token.ts
+++ src/qasm2/token.ts
@@ -121,7 +121,7 @@ function notParam(token: Token): boolean {
     token == Token.NNInteger ||
     token == Token.Real ||
     token == Token.Id ||
-    this.inverseLookup(token)
+    inverseLookup(token)
   ) {
     return false;
   }
