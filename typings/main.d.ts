3066880c8d5bc704872d691eb6f0c11a00ae8fa9
kamakiri01 <kamakiri01@users.noreply.github.com>
2026-02-16 19:29:53 +0900

    fix lint

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -3,30 +3,30 @@ import type * as qasm2 from "../dist/qasm2/ast";
 import type * as qasm3 from "../dist/qasm3/ast";
 
 export type Qasm2QuantumInstruction =
-    qasm2.Include |
-    qasm2.Version |
-    qasm2.QReg |
-    qasm2.CReg |
-    qasm2.Barrier |
-    qasm2.Measure |
-    qasm2.Gate |
-    qasm2.Variable |
-    qasm2.Opaque |
-    qasm2.If |
-    qasm2.Power |
-    qasm2.Divide |
-    qasm2.Times |
-    qasm2.Plus |
-    qasm2.Pi |
-    qasm2.Sin |
-    qasm2.Cos |
-    qasm2.Exp |
-    qasm2.Ln |
-    qasm2.Sqrt |
-    qasm2.Tan |
-    qasm2.NNInteger|
-    qasm2.Real |
-    qasm2.ApplyGate[];
+  qasm2.Include |
+  qasm2.Version |
+  qasm2.QReg |
+  qasm2.CReg |
+  qasm2.Barrier |
+  qasm2.Measure |
+  qasm2.Gate |
+  qasm2.Variable |
+  qasm2.Opaque |
+  qasm2.If |
+  qasm2.Power |
+  qasm2.Divide |
+  qasm2.Times |
+  qasm2.Plus |
+  qasm2.Pi |
+  qasm2.Sin |
+  qasm2.Cos |
+  qasm2.Exp |
+  qasm2.Ln |
+  qasm2.Sqrt |
+  qasm2.Tan |
+  qasm2.NNInteger |
+  qasm2.Real |
+  qasm2.ApplyGate[];
 
 
 export type Qasm3QuantumInstruction =
@@ -59,30 +59,29 @@ export type Qasm3QuantumInstruction =
   qasm3.SubroutineDefinition |
   qasm3.SubroutineCall |
   qasm3.BoxDefinition |
-  qasm3.BranchingStatement
-  ;
+  qasm3.BranchingStatement;
 
 export function parseString(
-    qasm: string,
-    version: number | OpenQASMVersion | OpenQASMMajorVersion,
-    verbose: boolean,
-    stringify: true
+  qasm: string,
+  version: number | OpenQASMVersion | OpenQASMMajorVersion,
+  verbose: boolean,
+  stringify: true
 ): string;
 
 export function parseString(
-    qasm: string,
-    version:
-        2 |
-        OpenQASMVersion & { major: OpenQASMMajorVersion.Version2 } |
-        OpenQASMMajorVersion.Version2
+  qasm: string,
+  version:
+    2 |
+    OpenQASMVersion & { major: OpenQASMMajorVersion.Version2 } |
+    OpenQASMMajorVersion.Version2
 ): Qasm2QuantumInstruction[];
 
 export function parseString(
-    qasm: string,
-    version:
-        3 |
-        OpenQASMVersion & { major: OpenQASMMajorVersion.Version3 } |
-        OpenQASMMajorVersion.Version3
+  qasm: string,
+  version:
+    3 |
+    OpenQASMVersion & { major: OpenQASMMajorVersion.Version3 } |
+    OpenQASMMajorVersion.Version3
 ): Qasm3QuantumInstruction[];
 
 export function parseString(
